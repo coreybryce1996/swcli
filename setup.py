ac3c9de7ff5f589631d726a61ba990130d35e626
coreybryce1996 <coreybryce1996@yahoo.com>
2019-08-16 13:17:01 -0400

    removing editable from install

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     py_modules=['starwars'],
     install_requires=[
         'Click',
-        'requests',
-        '--editable .'
+        'requests'
     ],
     entry_points='''
         [console_scripts]
